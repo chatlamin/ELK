43a3622d0b89ce99e424b83a2e123887246b6a13
chatlamin <viktorap@inbox.ru>
2021-01-27 14:54:48 +0300

    test label #unity-4

--- docker server/Kibana/run.sh
+++ docker server/Kibana/run.sh
@@ -16,7 +16,7 @@ cp -r ./container $PATH_CONF
 
 docker run \
     --name $NAME \
-    --label test=1234567911 \
+    --label test=123456791111 \
     --detach \
     --restart unless-stopped \
     --link elasticsearch:elasticsearch \
